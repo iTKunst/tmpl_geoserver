fa12448eb0201c63e14c6c32001f74e33144558d
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-26 15:25:59 -0400

    aa

--- docker/init.cmd
+++ docker/init.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 
-call :log_enter \DIR_PROJ\docker\init
+CALL LOG log_enter \DIR_PROJ\docker\init
 
 xcopy /q /y .\DIR_PROJ\docker\win\*.cmd .\Exe
 
-call :log_exit \DIR_PROJ\docker\init
+CALL LOG log_exit \DIR_PROJ\docker\init
--- env/init.cmd
+++ env/init.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 
-call :log_enter \Project\env\init
+CALL LOG log_enter \Project\env\init
 
 xcopy /q /y .\Project\env\win\*.cmd .\Env
 
-call :log_exit \Project\env\init
+CALL LOG log_exit \Project\env\init
